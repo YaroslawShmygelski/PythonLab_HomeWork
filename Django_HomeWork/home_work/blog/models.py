71309696be86f3acd857869d6a71f55701988c0c
Yaroslav <yaroslawsh04@gmail.com>
2024-07-02 18:01:09 +0200

    created User model

--- Django_HomeWork/home_work/blog/models.py
+++ Django_HomeWork/home_work/blog/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    bio = models.TextField(max_length=100, blank=True, null=True)
+    email = models.EmailField(unique=True, null=False, blank=False)

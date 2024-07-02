83c66353897fd0660a57eb176cdca0896fb32a96
Yaroslav <yaroslawsh04@gmail.com>
2024-07-02 18:22:39 +0200

    Created first migrations || Models added to DB

--- Django_HomeWork/home_work/home_work/settings.py
+++ Django_HomeWork/home_work/home_work/settings.py
@@ -33,12 +33,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django standard apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Site apps
+    'blog'
+
+
 ]
 
 MIDDLEWARE = [

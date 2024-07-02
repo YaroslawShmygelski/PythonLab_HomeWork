e58b7987974c8e486772c7c4108d7a4f4b0bbfc5
Yaroslav <yaroslawsh04@gmail.com>
2024-07-02 18:10:50 +0200

    Created model of the Post

--- Django_HomeWork/home_work/blog/models.py
+++ Django_HomeWork/home_work/blog/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class User(AbstractUser):
     bio = models.TextField(max_length=100, blank=True, null=True)
     email = models.EmailField(unique=True, null=False, blank=False)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20, blank=False, null=False)
+    content = models.TextField(max_length=100, blank=False, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.FileField(upload_to='photos/%Y/%m/%d/',
+                             blank=True, null=True, verbose_name="Photo")

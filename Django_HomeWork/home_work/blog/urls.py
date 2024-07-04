a9c2a465945e674d07cbbb88e603382808aeb536
Yaroslav <yaroslawsh04@gmail.com>
2024-07-04 14:34:08 +0200

    Created and styled home page with Posts displayed

--- Django_HomeWork/home_work/blog/urls.py
+++ Django_HomeWork/home_work/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.HomePageView.as_view(), name='home')
 ]
--- Django_HomeWork/home_work/blog/views.py
+++ Django_HomeWork/home_work/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import DetailView
+from django.views.generic.list import ListView
+
+from .models import Post
 
 
 # Create your views here.
 
-def index(request):
-    return render(request, template_name='blog/home.html')
+class HomePageView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/home.html'
+
+
